fe5f3c1ba7cfdf55b6a9acacf07bd2c3c1609a3b
oterte <97093479+oterte@users.noreply.github.com>
2023-02-23 10:04:07 +0900

    api 수정

--- src/api/todoApi.js
+++ src/api/todoApi.js
@@ -18,19 +18,19 @@ export const getTodos = async () => {
 // 어떤 todo를 추가하는지 알아야 하기 때문에 인자에 newTodo
 export const addTodo = async (newTodo) => {
     
-    await axios.post(`${process.env.REACT_APP_SERVER_URL}/todos`, newTodo)
+    await axios.post(`${process.env.REACT_APP_SERVER_URL}todos`, newTodo)
 }
 
 // todo 삭제
 export const deleteTodo = async (id) => {
-    await axios.delete(`${process.env.REACT_APP_SERVER_URL}/todos/${id}`)
+    await axios.delete(`${process.env.REACT_APP_SERVER_URL}todos/${id}`)
 } 
 // todo 토글
 
 export const toggleTodo = async ({id, toggleDone}) => {
     console.log(id)
     console.log(toggleDone)
-    await axios.patch(`${process.env.REACT_APP_SERVER_URL}/todos/${id}`,{
+    await axios.patch(`${process.env.REACT_APP_SERVER_URL}todos/${id}`,{
         isDone : toggleDone === false ? !toggleDone : !toggleDone
     })
 }
@@ -44,7 +44,7 @@ export const toggleTodo = async ({id, toggleDone}) => {
 export const updateTodoDesc = async ({id, changedesc}) => {
     console.log(id)
     console.log(changedesc)
-    await axios.patch(`${process.env.REACT_APP_SERVER_URL}/todos/${id}`, {
+    await axios.patch(`${process.env.REACT_APP_SERVER_URL}todos/${id}`, {
         desc : changedesc
     })
 }
